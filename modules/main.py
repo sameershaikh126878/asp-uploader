ec14e1997aef299c46c20873efca43873628a5f9
sameershaikh126878 <ywallah6@gmail.com>
2026-05-08 23:31:37 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -148,7 +148,7 @@ async def start(client: Client, msg: Message):
     await asyncio.sleep(1)
     await start_message.edit_text(
         Data.START.format(msg.from_user.mention) +
-        "Checking status Okay... Command is Private Dear.🌚**Bot Made BY @SunilChoudhary08**🔍\n\n"
+        "Checking status Okay... Command is Private Dear.🌚**Bot Made BY♡︎⎯꯭̽𝅃꯭᳚🦋꯭ ∂я-αѕρ 𝅥ͦ𝆬⋆𝆹𝅥𝅮᪳🪽**🔍\n\n"
         "Progress:[🟩🟩🟩🟩🟩🟩🟩🟩🟩] 100%\n\n"
     )
 
@@ -158,9 +158,9 @@ async def restart_handler(_, m):
     os.execl(sys.executable, sys.executable, *sys.argv)
 
 
-@bot.on_message(filters.command(["The08"]) )
+@bot.on_message(filters.command(["TOX"]) )
 async def txt_handler(bot: Client, m: Message):
-    editable = await m.reply_text(f"**🔹Hi I am Poweful Lovely TXT Downloader📥 Bot.**\n🔹**Send me the TXT file and Just wait and Watch😚.**")
+    editable = await m.reply_text(f"**🔹Hi I am Poweful Lovely TXT Downloader📥 Bot.**\n🔹**Send me the TXT file and Just wait and Watch😚Comd /tox.**")
     input: Message = await bot.listen(editable.chat.id)
     x = await input.download()
     await input.delete(True)
@@ -219,7 +219,7 @@ async def txt_handler(bot: Client, m: Message):
     except Exception:
             res = "UN"
     
-    await editable.edit("**Enter Your Name or send '/SK' for use default.🌚\n Eg :@SunilChoudhary08 **")
+    await editable.edit("**Enter Your Name or send '/SK' for use default.🌚\n Eg ♡︎⎯꯭̽𝅃꯭᳚🦋꯭ ∂я-αѕρ 𝅥ͦ𝆬⋆𝆹𝅥𝅮᪳🪽 **")
     input3: Message = await bot.listen(editable.chat.id)
     raw_text3 = input3.text
     await input3.delete(True)
@@ -228,7 +228,7 @@ async def txt_handler(bot: Client, m: Message):
     else:
         CR = raw_text3
         
-    await editable.edit("**Enter Your PW Token For 𝐌𝐏𝐃 𝐔𝐑𝐋  or send '/SK' for use default🎀**")
+    await editable.edit("**Enter Your PW Token For 𝐌𝐏𝐃 𝐔𝐑𝐋  or send '/ASP' for use default🎀**")
     input4: Message = await bot.listen(editable.chat.id)
     raw_text4 = input4.text
     await input4.delete(True)
@@ -311,8 +311,8 @@ async def txt_handler(bot: Client, m: Message):
 
             try:  
                 
-                cc = f'**📹 VID_ID: {str(count).zfill(3)}.\n\n📝 Title: {name1} {res}.mkv\n\n<pre><code>📚 Batch Name: {b_name}</code></pre>\n\n📥 Extracted By♠ : {CR}\n\n**👑━━━🤍 𝑻𝒉𝒆 𝑺𝑲 🤍━━━👑**'
-                cc1 = f'**💾 PDF_ID: {str(count).zfill(3)}.\n\n📝 Title: {name1} .pdf\n\n<pre><code>📚 Batch Name: {b_name}</code></pre>\n\n📥 Extracted By♠ : {CR}\n\n**👑━━━🤍 𝑻𝒉𝒆 𝑺𝑲 🤍━━━👑**'
+                cc = f'**📹 VID_ID: {str(count).zfill(3)}.\n\n📝𝐿𝐸𝒞 𝒯𝐼𝒯𝐿𝐸: {name1} {res}.mkv\n\n<pre><code>📚𝑩𝑨𝑻𝑪𝑯 𝑵𝑨𝑴𝑬: {b_name}</code></pre>\n\n📥𝐃𝐎𝐖𝐍𝐋𝐎𝐀𝐃 𝐁𝐘 ♠ : {CR}\n\n**👑━━━🤍 𝐌𝐑 𝐓𝐎𝐗𝐈𝐂🤍━━━👑**'
+                cc1 = f'**💾 PDF_ID: {str(count).zfill(3)}.\n\n📝𝒫𝒟𝐹 𝒯𝐼𝒯𝐿𝐸: {name1} .pdf\n\n<pre><code>📚𝑩𝑨𝑻𝑪𝑯 𝑵𝑨𝑴𝑬: {b_name}</code></pre>\n\n📥𝐃𝐎𝐖𝐍𝐋𝐎𝐀𝐃 𝐁𝐘 ♠ : {CR}\n\n**👑━━━🤍𝐌𝐑 𝐓𝐎𝐗𝐈𝐂🤍━━━👑**'
                     
                 
                 if "drive" in url:
@@ -374,7 +374,7 @@ async def txt_handler(bot: Client, m: Message):
                         continue                       
                           
                 else:
-                    Show = f"✰🖥️ 𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝𝐢𝐧𝐠 𝗪𝗮𝗶𝘁..🤖🚀 »\n\n📝 Title:- `{name}\n\n📹 𝐐𝐮𝐥𝐢𝐭𝐲 » {raw_text2}`\n\n**🔗 𝐔𝐑𝐋 »** `{url}`\n\n**𝐁𝐨𝐭 𝐌𝐚𝐝𝐞 𝐁𝐲🧸: ✦ @SunilChoudhary08 ❖"
+                    Show = f"✰🖥️ 𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝𝐢𝐧𝐠 𝗪𝗮𝗶𝘁..🤖🚀 »\n\n📝 Title:- `{name}\n\n📹 𝐐𝐮𝐥𝐢𝐭𝐲 » {raw_text2}`\n\n**🔗 𝐔𝐑𝐋 »** `{url}`\n\n**𝐁𝐨𝐭 𝐌𝐚𝐝𝐞 𝐁𝐲🧸: ✦♡︎⎯꯭̽𝅃꯭᳚🦋꯭ ∂я-αѕρ 𝅥ͦ𝆬⋆𝆹𝅥𝅮᪳🪽❖"
                     prog = await m.reply_text(Show)
                     res_file = await helper.download_video(url, cmd, name)
                     filename = res_file
@@ -545,8 +545,8 @@ async def txt_handler(bot: Client, m: Message):
 
             try:  
         
-                cc = f'**📹 VID_ID: {str(count).zfill(3)}.\n\nTitle: {name1} STUDENTS💛{res}.mkv\n\n📚 Batch Name: {b_name}\n\n📥 Extracted By♠ : {CR}\n\n**👑━━━🤍 𝑻𝒉𝒆 𝑺𝑲 🤍━━━👑**'
-                cc1 = f'**💾 PDF_ID: {str(count).zfill(3)}.\n\nTitle: {name1} STUDENTS💛.pdf\n\n📚 Batch Name: {b_name}\n\n📥 Extracted By♠ : {CR}\n\n**👑━━━🤍 𝑻𝒉𝒆 𝑺𝑲 🤍━━━👑**'
+                cc = f'**📹 VID_ID: {str(count).zfill(3)}.\n\nTitle: {name1} STUDENTS💛{res}.mkv\n\n📚 Batch Name: {b_name}\n\n📥 Extracted By♠ : {CR}\n\n**👑━━━🤍MR TOXIC🤍━━━👑**'
+                cc1 = f'**💾 PDF_ID: {str(count).zfill(3)}.\n\nTitle: {name1} STUDENTS💛.pdf\n\n📚 Batch Name: {b_name}\n\n📥 Extracted By♠ : {CR}\n\n**👑━━━🤍MR TOXIC🤍━━━👑**'
                     
                 
                 if "drive" in url:
@@ -608,7 +608,7 @@ async def txt_handler(bot: Client, m: Message):
                         continue                       
                           
                 else:
-                    Show = f"✰🖥️𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝𝐢𝐧𝐠 𝗪𝗮𝗶𝘁..🤖🚀»\n\n📝 Title:- `{name}\n\n🖥️ 𝐐𝐮𝐥𝐢𝐭𝐲 » {raw_text2}`\n\n**🔗 𝐔𝐑𝐋 »** `{url}`\n\n**𝐁𝐨𝐭 𝐌𝐚𝐝𝐞 𝐁𝐲🧸: ✦ @SunilChoudhary08✰"
+                    Show = f"✰🖥️𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝𝐢𝐧𝐠 𝗪𝗮𝗶𝘁..🤖🚀»\n\n📝 Title:- `{name}\n\n🖥️ 𝐐𝐮𝐥𝐢𝐭𝐲 » {raw_text2}`\n\n**🔗 𝐔𝐑𝐋 »** `{url}`\n\n**𝐁𝐨𝐭 𝐌𝐚𝐝𝐞 𝐁𝐲🧸: ✦♡︎⎯꯭̽𝅃꯭᳚🦋꯭ ∂я-αѕρ 𝅥ͦ𝆬⋆𝆹𝅥𝅮᪳🪽✰"
                     prog = await m.reply_text(Show)
                     res_file = await helper.download_video(url, cmd, name)
                     filename = res_file
@@ -625,7 +625,7 @@ async def txt_handler(bot: Client, m: Message):
 
     except Exception as e:
         await m.reply_text(e)
-    await m.reply_text("𝐀𝐋𝐋 𝐃𝐎𝐍𝐄 REACTIONS khud doge ya kahna padega .✅🔸")
+    await m.reply_text("𝐀𝐋𝐋 𝐃𝐎𝐍𝐄 𝐋𝐀𝐃𝐋𝐄 𝐌𝐈𝐘𝐀𝐎𝐍 𝐆𝐇𝐎𝐏 𝐆𝐇𝐎𝐏.✅🔸")
 
 
 
